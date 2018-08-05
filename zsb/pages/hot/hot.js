e403c9253dc6cc9a3d4268d7186d85c29a433a45
minyi <minyi@mobvoi.com>
2018-08-05 15:54:57 +0800

    zsb index page

--- zsb/pages/hot/hot.js
+++ zsb/pages/hot/hot.js
@@ -1,66 +1,80 @@
-// pages/hot/hot.js
+//index.js
+//获取应用实例
+const app = getApp()
+const request = require('../../utils/request.js');
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    home: {},
+    imgUrls: [],
+    indicatorDots: false,
+    autoplay: true,
+    interval: 6000,
+    duration: 2000,
+    schoolList: [],
+    bgColor: '',
+    college_course: []
   },
-
   /**
-   * 生命周期函数--监听页面加载
-   */
+  * 生命周期函数--监听页面加载
+  */
   onLoad: function (options) {
-  
+    console.log('aaa');
+    this.getHome();
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  // 去学院详情
+  navigatoSchool: function (event) {
+    var id = event.target.dataset.id;
+    wx.navigateTo({
+      url: '/pages/school-detail/school-detail?id=' + id,
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+  navigatoSchools: function (event) {
+    var id = event.currentTarget.dataset.id;
+    wx.navigateTo({
+      url: '/pages/school-detail/school-detail?id=' + id,
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
+  // 去课程页
+  navigateToCourse: function (event) {
+    var id = event.currentTarget.dataset.id;
+    wx.navigateTo({
+      url: '/pages/course-detail/course-detail?id=' + id
+    })
   },
+  // 请求首页
+  getHome: function () {
+    var that = this;
+    console.log('bbb');
+    request.getHome(function (res) {
+      console.log(res);
+      var data = res.data;
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
+      // 数据处理
+      var imgUrls = data.ads.map(function (item) {
+        return item.img;
+      });
+      data.colleges.forEach(function (item, index) {
+        item.bgColor = that.getSchoolColor(index);
+      })
+      console.log(data);
+      that.setData({
+        imgUrls: imgUrls,
+        home: data,
+        college_course: data.college_course
+      });
+    })
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  // 动态生成学院背景颜色
+  getSchoolColor: function (index) {
+    switch (index) {
+      case 0: return 'bc_green';
+      case 1: return 'bc_yellow';
+      case 2: return 'bg_purper';
+      case 3: return 'bc_blue';
+      case 4: return 'bc_red';
+      case 5: return 'bg_music';
+      case 6: return 'bg_wenxue';
+      default: return 'bc_blue'
+    }
   }
 })
